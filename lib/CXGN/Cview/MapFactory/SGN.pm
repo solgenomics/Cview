592fce1c88a1e33275ef19e15bd11cead51d09af
Rob Buels <rmb32@cornell.edu>
2010-03-11 20:24:05 +0000

    whitespace cleanup
    git-svn-id: svn+ssh://svn.sgn.cornell.edu/cxgn/Cview/trunk@17608 b4ddb4e3-33f1-0310-aa89-e703427ae9b8

--- lib/CXGN/Cview/MapFactory/SGN.pm
+++ lib/CXGN/Cview/MapFactory/SGN.pm
@@ -1,20 +1,17 @@
-
-
-
 =head1 NAME
 
 CXGN::Cview::MapFactory - a factory object for CXGN::Cview::Map objects
-           
+
 =head1 SYNOPSYS
 
 my $map_factory  = CXGN::Cview::MapFactory->new($dbh);
 $map = $map_factory->create({map_version_id=>"u1"});
-         
+
 =head1 DESCRIPTION
 
 The MapFactory object is part of a compatibility layer that defines the data sources of the comparative mapviewer. If there are different types of maps that can be distinguished by their ids, the MapFactory should be implemented to return the right map object for the given id. Of course, the corresponding map object also needs to be implemented, using the interface defined in CXGN::Cview::Map .
 
-The MapFactory constructor takes a database handle (preferably constructed using CXGN::DB::Connection object). The map objects can then be constructed using the create function, which takes a hashref as a parameter, containing either map_id or map_version_id as a key (but not both). map_ids will be converted to map_version_ids immediately. Map_version_ids are then analyzed and depending on its format, CXGN::Cview::Map object of the proper type is returned. 
+The MapFactory constructor takes a database handle (preferably constructed using CXGN::DB::Connection object). The map objects can then be constructed using the create function, which takes a hashref as a parameter, containing either map_id or map_version_id as a key (but not both). map_ids will be converted to map_version_ids immediately. Map_version_ids are then analyzed and depending on its format, CXGN::Cview::Map object of the proper type is returned.
 
 The function get_all_maps returns all maps as list of appropriate CXGN::Cview::Map::* objects.
 
@@ -34,7 +31,7 @@ For the current SGN implementation, the following identifier formats are defined
  p\d+      CXGN::Cview::Map::SGN::Physical
  c\d+      CXGN::Cview::Map::SGN::Contig
  o         CXGN::Cview::Map::SGN::ProjectStats map object
- 
+
 The actual map objects returned are defined in the CXGN::Cview::Maps namespace. Because this is really a compatibility layer, an additional namespace of the resource is appended, such that a genetic map at SGN could be defined as CXGN::Cview::Maps::SGN::Genetic . If no corresponding map is found, undef is returned.
 
 =head1 AUTHOR(S)
@@ -42,7 +39,7 @@ The actual map objects returned are defined in the CXGN::Cview::Maps namespace. 
 Lukas Mueller (lam87@cornell.edu)
 
 =head1 VERSION
- 
+
 1.0, March 2007
 
 =head1 LICENSE
@@ -87,7 +84,7 @@ use CXGN::Cview::Map::SGN::Contig;
 sub new {
     my $class = shift;
     my $dbh = shift;
-    
+
     my $self = $class->SUPER::new($dbh);
 
     return $self;
@@ -95,15 +92,15 @@ sub new {
 
 =head2 function create()
 
-  Description:  creates a map based on the hashref given, which 
-                should either contain the key map_id or map_version_id 
+  Description:  creates a map based on the hashref given, which
+                should either contain the key map_id or map_version_id
                 and an appropriate identifier. The function returns undef
                 if a map of the given id cannot be found/created.
-  Example:      
+  Example:
 
 =cut
 
-sub create { 
+sub create {
     my $self = shift;
     my $hashref = shift;
 
@@ -113,93 +110,93 @@ sub create {
     croak "temp_dir '$temp_dir' does not exist, and could not create"
         unless -d $temp_dir || File::Path::mkpath( $tempdir );
 
-    if (!exists($hashref->{map_id}) && !exists($hashref->{map_version_id})) { 
-	die "[CXGN::Cview::MapFactory] Need either a map_id or map_version_id.\n"; 
+    if (!exists($hashref->{map_id}) && !exists($hashref->{map_version_id})) {
+	die "[CXGN::Cview::MapFactory] Need either a map_id or map_version_id.\n";
     }
-    if ($hashref->{map_id} && $hashref->{map_version_id}) { 
+    if ($hashref->{map_id} && $hashref->{map_version_id}) {
 	die "[CXGN::Cview::MapFactory] Need either a map_id or map_version_id - not both.\n";
     }
-    if ($hashref->{map_id}) { 
-	$hashref->{map_version_id}=CXGN::Cview::Map::Tools::find_current_version($self->get_dbh(), $hashref->{map_id}); 
+    if ($hashref->{map_id}) {
+	$hashref->{map_version_id}=CXGN::Cview::Map::Tools::find_current_version($self->get_dbh(), $hashref->{map_id});
     }
-    
+
     # now, we only deal with map_versions...
     #
     my $id = $hashref->{map_version_id};
 
     #print STDERR "MapFactory: dealing with id = $id\n";
 
-    # if the map_version_id is purely numeric, 
+    # if the map_version_id is purely numeric,
     # check if the map is in the maps table and generate the
     # appropriate map
 
-    if ($id=~/^\d+$/) { 
+    if ($id=~/^\d+$/) {
 	my $query = "SELECT map_version_id, map_type, map_id, short_name FROM sgn.map join sgn.map_version using(map_id) WHERE map_version_id=?";
 	my $sth = $self->get_dbh()->prepare($query);
 	$sth->execute($id);
 	my ($id, $map_type) = $sth->fetchrow_array();
-	if ($map_type =~ /genetic/i) { 
+	if ($map_type =~ /genetic/i) {
 	    return CXGN::Cview::Map::SGN::Genetic->new($self->get_dbh(), $id);
 	}
-	elsif ($map_type =~ /fish/) { 
+	elsif ($map_type =~ /fish/) {
 	    #print STDERR "Creating a fish map...\n";
 	    return CXGN::Cview::Map::SGN::Fish->new($self->get_dbh(), $id);
 	}
-	elsif ($map_type =~ /seq/) { 
+	elsif ($map_type =~ /seq/) {
 	    #print STDERR "Creating a seq map...\n";
 	    return CXGN::Cview::Map::SGN::Sequence->new($self->get_dbh(), $id);
 	}
     }
-    elsif ($id =~ /^u/i) { 
+    elsif ($id =~ /^u/i) {
 	return CXGN::Cview::Map::SGN::User->new($self->get_dbh(), $id);
     }
-    elsif ($id =~ /^il/i) { 
-	my $abstract = 
+    elsif ($id =~ /^il/i) {
+	my $abstract =
 	"The tomato Introgression lines (ILs) are a set of nearly isogenic lines (NILs) developed by Dani Zamir through a succession of backcrosses, where each line carries a single genetically defined chromosome segment from a divergent genome. The ILs, representing whole-genome coverage of S. pennellii in overlapping segments in the genetic background of S. lycopersicum cv. M82, were first phenotyped in 1993, and presently this library consists of 76 genotypes. ";
-	
+
 	my ($population_id, $map_id) = $self->get_db_ids($id);
-	if ($map_id == 9) { 
+	if ($map_id == 9) {
 	    $abstract .= " This IL map is based on markers of the F2-2000 map. ILs have also been mapped <a href=\"map.pl?map_id=il6.5&amp;show_ruler=1&amp;show_offsets=1\" >with the ExPEN1992 map as a reference</a>.";
 	}
-	elsif ($map_id ==5) { 
+	elsif ($map_id ==5) {
 	    $abstract .= " The IL lines on this map have been characterized based on the markers on the 1992 tomato map. ILs have also been mapped <a href=\"map.pl?map_id=il6.9&amp;show_ruler=1&amp;show_offsets=1\" >with the ExPEN2000 map as a reference</a>. ";
 	}
-	
+
 	my $ref_map = "ExPEN2000";
 	if ($id==5) { $ref_map = "ExPEN1992";}
 	my $long_name =qq | <i>Solanum lycopersicum</i> Zamir Introgression Lines (IL) based on $ref_map |;
 
-	return CXGN::Cview::Map::SGN::IL->new($self->get_dbh(), $id, 
+	return CXGN::Cview::Map::SGN::IL->new($self->get_dbh(), $id,
 					      { short_name => "Tomato IL map",
 						long_name => $long_name,
-						abstract => $abstract 
+						abstract => $abstract
 					      });
     }
-    elsif ($id =~ /^\//) { 
+    elsif ($id =~ /^\//) {
 	#return CXGN::Cview::Map::SGN::File->new($dbh, $id);
     }
-    elsif ($id =~ /^p\d+/) { 
+    elsif ($id =~ /^p\d+/) {
 	return CXGN::Cview::Map::SGN::Physical->new($self->get_dbh(), $id);
     }
-    elsif ($id =~ /^o$/i) { 
-	
-	return CXGN::Cview::Map::SGN::ProjectStats->new($self->get_dbh(), { 
+    elsif ($id =~ /^o$/i) {
+
+	return CXGN::Cview::Map::SGN::ProjectStats->new($self->get_dbh(), {
 	    short_name=>"Tomato Sequencing Progress",
-	    long_name=>"Tomato Sequencing Statistics by Chromosome", 
+	    long_name=>"Tomato Sequencing Statistics by Chromosome",
 	    abstract => $self->get_abstract(),
-	    
+
 	});
     }
-    
-    elsif ($id =~ /^agp$/i) { 
+
+    elsif ($id =~ /^agp$/i) {
 	return CXGN::Cview::Map::SGN::AGP->new($self->get_dbh(), $id, {
 					       short_name => "Tomato AGP map",
 					       long_name => "Tomato (Solanum lycopersicum) Accessioned Golden Path map",
 					       abstract => "<p>The AGP map shows the sequencing progress of the international tomato genome sequencing project by listing all finished clones by estimated physical map position . Each sequencing center generates one or more AGP (Accessioned Golden Path) files and uploads them to SGN. These files contain all the sequenced BACs, their position on the chromosome, the overlaps with other BACs and other information. For a complete specification, please refer to the <a href=\"http://www.sanger.ac.uk/Projects/C_elegans/DOCS/agp_files.shtml\">Sanger AGP specification</a>. The AGP files can also be downloaded from the SGN FTP site, at <a href=\"ftp://ftp.sgn.cornell.edu/tomato_genome/agp/\">ftp://ftp.sgn.cornell.edu/tomato_genome/agp/</a>.</p> <p>Note that this map is in testing (beta), and not all features may be functional.</p>" ,
 					       temp_dir => $temp_dir } );
     }
-    elsif ($id =~ /^itag$/i) { 
-	return CXGN::Cview::Map::SGN::ITAG->new($self->get_dbh(), $id, { 
+    elsif ($id =~ /^itag$/i) {
+	return CXGN::Cview::Map::SGN::ITAG->new($self->get_dbh(), $id, {
 						short_name => "Tomato ITAG map",
 						long_name=>"Tomato (Solanum lycopersicum) ITAG map",
 						abstract=>"<p>The ITAG map shows the contig assembly and the corresponding BACs as used by the most recent annotation from the International Tomato Annotation Group (ITAG, see <a href=\"http://www.ab.wur.nl/TomatoWiki\">ITAG Wiki</a>). Clicking on the contigs will show the ITAG annotation in the genome browser.",
@@ -209,32 +206,32 @@ sub create {
 #    elsif ($id =~ /^u\d+$/i) {
 #	return CXGN::Cview::Map::SGN::User->new($self->get_dbh(), $id);
 #    }
-    elsif ($id =~ /^c\d+$/i) { 
-	return CXGN::Cview::Map::SGN::Contig->new($self->get_dbh(), $id, { 
+    elsif ($id =~ /^c\d+$/i) {
+	return CXGN::Cview::Map::SGN::Contig->new($self->get_dbh(), $id, {
 	    berkeley_db_path=>'/data/prod/public/tomato_genome/physical_mapping/fpc/SGN_2009/gbrowse/curr/',
 	    short_name => "Tomato FPC map SGN2009",
 	    long_name => "Solanum lycopersicum Contig Map SGN2009",
 	    temp_dir => $temp_dir,
 	    marker_link => "/gbrowse/gbrowse/fpc_tomato_sgn_2009/?name=",
-	    
+
 	    abstract => qq|
-	    
+
 <p>This map shows the contig positions of the SGN2009 physical map constructed at the Arizona Genome Institute in late 2009. The marker positions shown are from the latest <a href="/cview/map.pl?map_id=9">EXPEN2000 map</a>.</p>
-    
+
     <p>This physical map contains clones from the HindIII, EcoRI, MboI and sheared BAC library.</p>
-    
+
     <p>This overview shows the counts of contigs along the chromosome. Click on any chromosome to view the individual contigs. More information on each contig can be obtained by by clicking on a specific contig.
-    
+
     <p>Specific contig IDs, including contigs that are not mapped, can be searched on the <a href="/gbrowse/gbrowse/fpc_tomato_sgn_2009/">FPC viewer page</a>.</p>
     <br />
-    
-    
+
+
 |
-						  
+
 						  }
 	    );
     }
-    
+
     warn "Map NOT FOUND!!!!!!!!!!!!!!!!!!\n\n";
     return undef;
 
@@ -242,12 +239,12 @@ sub create {
 
 =head2 function get_all_maps()
 
-  Synopsis:	
+  Synopsis:
   Arguments:	none
-  Returns:	a list of all maps currently defined, as 
+  Returns:	a list of all maps currently defined, as
                 CXGN::Cview::Map objects (and subclasses)
   Side effects:	Queries the database for certain maps
-  Description:	
+  Description:
 
 =cut
 
@@ -258,14 +255,14 @@ sub get_all_maps {
     my @user_maps = $self->get_user_maps();
     my @maps = (@system_maps, @user_maps);
     return @maps;
-    
+
 }
 
 
 =head2 get_system_maps
 
   Usage:        my @system_maps = $map_factory->get_system_maps();
-  Desc:         retrieves a list of system maps (from the sgn 
+  Desc:         retrieves a list of system maps (from the sgn
                 database) as a list of CXGN::Cview::Map objects
   Ret:
   Args:
@@ -278,19 +275,19 @@ sub get_system_maps {
     my $self = shift;
 
     my @maps = ();
-    
+
     my $query = "SELECT map.map_id FROM sgn.map LEFT JOIN sgn.map_version USING(map_id) LEFT JOIN sgn.accession on(parent_1=accession.accession_id) LEFT JOIN sgn.organism USING(organism_id) LEFT JOIN common_name USING(common_name_id) WHERE current_version='t' ORDER by common_name.common_name";
     my $sth = $self->get_dbh()->prepare($query);
     $sth->execute();
 
-    while (my ($map_id) = $sth->fetchrow_array()) { 
+    while (my ($map_id) = $sth->fetchrow_array()) {
 	my $map = $self->create({ map_id => $map_id });
 	if ($map) { push @maps, $map; }
     }
 
     # push il, physical, contig, and agp map
     #
-    foreach my $id ("il6.5", "il6.9", "p9", "c9", "agp", "itag") { 
+    foreach my $id ("il6.5", "il6.9", "p9", "c9", "agp", "itag") {
 	my $map = $self->create( {map_id=>$id} );
 	if ($map) { push @maps, $map; }
     }
@@ -301,7 +298,7 @@ sub get_system_maps {
 
 
 =head2 get_user_maps
- 
+
  Status:       DEPRECATED. Does nothing now, as user maps have been disabled.
  Usage:
  Desc:         retrieves the current user maps of the logged in user.
@@ -319,13 +316,13 @@ sub get_user_maps {
     my @maps = ();
 #     my $login = CXGN::Login->new($self->get_dbh());
 #     my $user_id = $login->has_session();
-#     if ($user_id) { 
+#     if ($user_id) {
 # 	my $q3 = "SELECT user_map_id FROM sgn_people.user_map WHERE obsolete='f' AND sp_person_id=?";
 # 	my $h3 = $self->get_dbh()->prepare($q3);
 # 	$h3->execute($user_id);
-# 	while (my ($user_map_id) = $h3->fetchrow_array()) { 
+# 	while (my ($user_map_id) = $h3->fetchrow_array()) {
 # 	    my $map = $self->create( {map_id=>"u".$user_map_id} );
-	    
+
 # 	    if ($map) { push @maps, $map; }
 # 	}
 #     }
@@ -333,14 +330,14 @@ sub get_user_maps {
 }
 
 
-sub get_db_ids { 
+sub get_db_ids {
     my $self = shift;
     my $id = shift;
 
     my $population_id = 6;
     my $reference_map_id=5;
 
-    if ($id=~/il(\d+)\.?(\d*)?/) { 
+    if ($id=~/il(\d+)\.?(\d*)?/) {
 	$population_id=$1;
 	$reference_map_id=$2;
     }
