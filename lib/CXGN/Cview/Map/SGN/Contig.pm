8565c9fe5ee497eaa18f3bdcc747c479ffdbdd20
Rob Buels <rmb32@cornell.edu>
2010-03-11 20:36:18 +0000

    whitespace cleanup
    git-svn-id: svn+ssh://svn.sgn.cornell.edu/cxgn/Cview/trunk@17612 b4ddb4e3-33f1-0310-aa89-e703427ae9b8

--- lib/CXGN/Cview/Map/SGN/Contig.pm
+++ lib/CXGN/Cview/Map/SGN/Contig.pm
@@ -1,16 +1,13 @@
-
-
-
 =head1 NAME
 
-CXGN::Cview::Map::SGN::Contig - a class to generate maps of contigs aligned to the genome           
-           
+CXGN::Cview::Map::SGN::Contig - a class to generate maps of contigs aligned to the genome
+
 =head1 SYNOPSYS
 
  my $map = CXGN::Cview::Map::SGN::Contig->new($dbh, $id);
  my $chr = $map->get_chromosome(1);
  # etc...
-      
+
 =head1 DESCRIPTION
 
 The FPC data is currently stored in a berkeleydb database. It is accessed using the following methods:
@@ -30,9 +27,8 @@ This class implements the following functions:
 
 =cut
 
-use strict;
-
 package CXGN::Cview::Map::SGN::Contig;
+use strict;
 
 use Bio::DB::GFF;
 use File::Spec;
@@ -45,14 +41,14 @@ use base qw | CXGN::Cview::Map::SGN::Genetic |;
 
 =head2 function new
 
-  Synopsis:	
+  Synopsis:
   Arguments:	a database handle (preferably generated through
                 CXGN::DB::Connection) and a map id. Currently,
                 only one map_id is supported, with the alpha-
                 numeric id of "contig".
-  Returns:	
-  Side effects:	
-  Description:	
+  Returns:
+  Side effects:
+  Description:
 
 =cut
 
@@ -61,7 +57,7 @@ sub new {
     my $dbh = shift;
     my $id = shift;
     my $args = shift;
-    
+
     my $db_version_id = get_db_id($dbh, $id);
     my $self = $class -> SUPER::new($dbh, $db_version_id);
 
@@ -75,7 +71,7 @@ sub new {
     $self->{marker_link} = $args->{marker_link};
     $self->set_id($id);
 
-    
+
 
     return $self;
 }
@@ -83,11 +79,11 @@ sub new {
 
 =head2 function get_chromosome()
 
-  Synopsis:	
-  Arguments:	
-  Returns:	
-  Side effects:	
-  Description:	
+  Synopsis:
+  Arguments:
+  Returns:
+  Side effects:
+  Description:
 
 =cut
 
@@ -111,28 +107,28 @@ sub get_chromosome {
 	);
 
 
-    foreach my $m ($genetic->get_markers()) { 
+    foreach my $m ($genetic->get_markers()) {
 	$m->set_chromosome($chromosome);
 	$chromosome->add_marker($m);
 	my $offset = $m->get_offset();
-	if ($offset > $largest_offset) { 
+	if ($offset > $largest_offset) {
 	    $largest_offset=$offset;
 	}
 	$m->hide();
-	
+
 	my @gff_markers = $gff->features(-method => 'marker',
 		       -attributes => { Name => $m->get_name() },
 		       );
 	my @contigs = ();
-	foreach my $gm (@gff_markers) { 
+	foreach my $gm (@gff_markers) {
 	    @contigs = $gm->refseq();
 	}
 	my $count = 0;
-	foreach my $c (@contigs) { 
+	foreach my $c (@contigs) {
 	    my $contig = CXGN::Cview::Marker::Physical->new();
 	    $contig->set_chromosome($chromosome);
 	    $contig->set_name($c);
-	    
+
 	    #my $url = "/gbrowse/gbrowse/sanger_tomato_fpc/?name=$c";
 	    my $url = $self->{marker_link};
 	    $contig->set_marker_name($c);
@@ -144,52 +140,52 @@ sub get_chromosome {
 	    $contig->set_tooltip("Contig: $c. Anchored to: ".($m->get_name()).".");
 	    $chromosome -> add_marker($contig);
 	    $count++;
-	}   
+	}
     }
     $chromosome->set_length($largest_offset);
     $self->{chr}->[$chr_nr]=$chromosome;
     return $chromosome;
-   
+
 }
 
 =head2 function get_overview_chromosome()
 
-  Synopsis:	
-  Arguments:	
-  Returns:	
-  Side effects:	
-  Description:	
+  Synopsis:
+  Arguments:
+  Returns:
+  Side effects:
+  Description:
 
 =cut
 
 sub get_overview_chromosome {
     my $self = shift;
     my $chr_nr = shift;
-    
+
     my $bargraph = CXGN::Cview::Chromosome::BarGraph->new();
 
     my $largest_offset = 0;
-    
+
     my $chromosome = $self->get_chromosome($chr_nr);
-    
-    foreach my $m ($chromosome->get_markers()) { 
-	if ($m->get_marker_type() eq "contig") { 
-	    
+
+    foreach my $m ($chromosome->get_markers()) {
+	if ($m->get_marker_type() eq "contig") {
+
 	    my $offset = $m->get_offset();
 	    $bargraph -> add_association("manual", $offset, 1);
 	    if ($offset>$largest_offset) { $largest_offset = $offset; }
 	}
-    }   
+    }
     return $bargraph;
 }
 
 =head2 function get_chromosome_connections()
 
-  Synopsis:	
-  Arguments:	
-  Returns:	
-  Side effects:	
-  Description:	
+  Synopsis:
+  Arguments:
+  Returns:
+  Side effects:
+  Description:
 
 =cut
 
@@ -202,7 +198,7 @@ sub get_chromosome_connections {
     return @list;
 }
 
-sub get_db_id { 
+sub get_db_id {
     my $dbh = shift;
     my $id = shift;
     my $db_id = $id;
@@ -211,69 +207,69 @@ sub get_db_id {
 }
 
 
-sub can_zoom { 
+sub can_zoom {
     return 0;
 }
 
-sub get_marker_link { 
+sub get_marker_link {
     my $self = shift;
-    my $clone_id= shift; 
+    my $clone_id= shift;
     if ($clone_id) { return ""; }
     else { return ""; }
 }
 
-sub get_marker_count { 
+sub get_marker_count {
     my $self = shift;
     my $chr_nr = shift;
 
     my $tmp_file = "";
 
-    my @lengths = $self->cache_marker_counts(); 
+    my @lengths = $self->cache_marker_counts();
     return $lengths[$chr_nr-1];
 }
 
-sub cache_marker_counts { 
+sub cache_marker_counts {
     my $self = shift;
-    
+
     my @lengths = ();
     my $temp_file = File::Spec->catfile($self->{temp_dir}, 'contig'.$self->get_id()."_marker_counts.txt");
-					
-    if (! -e ($temp_file)) { 
+
+    if (! -e ($temp_file)) {
 	open(my $TEMP, ">$temp_file") || die "Can't open $temp_file for writing.";
-	for my $c (1..12) { 
+	for my $c (1..12) {
 	    my $count = 0;
 	    my $chr = $self->get_chromosome($c);
-	    foreach my $m ($chr->get_markers()) { 
-		if ($m->get_marker_name()=~ /^ctg/) { 
+	    foreach my $m ($chr->get_markers()) {
+		if ($m->get_marker_name()=~ /^ctg/) {
 		    $count++;
 		}
 	    }
 	    print $TEMP "$c\t$count\n";
-	    
+
 	}
     }
-	
-    else { 
+
+    else {
 	open(my $TEMP, "<$temp_file") || die "Can't open $temp_file for reading.";
-	while (<$TEMP>) { 
+	while (<$TEMP>) {
 	    chomp;
 	    my ($c, $length) = split /\t/;
 	    push @lengths, $length;
 	}
-	
-    }    
+
+    }
     return @lengths;
 }
 
 
-sub get_map_stats { 
+sub get_map_stats {
     my $self = shift;
 
     my $count = 0;
-    foreach my $c (1..12) { 
+    foreach my $c (1..12) {
 	$count += $self->get_marker_count($c);
     }
-    
+
     return "$count contigs have been assigned to this map";
 
 
