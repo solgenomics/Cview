4f9c0e6d1212bad41c71e1f1e3fc017e3a6ad809
mueller <lam87@cornell.edu>
2011-02-22 23:59:54 -0500

    A new map type, consisting of images, such as pachytene micrographs, overlaid on the map.

---
+++ lib/CXGN/Cview/Chromosome/Image.pm
@@ -0,0 +1,53 @@
+
+use Modern::Perl;
+
+package CXGN::Cview::Chromosome::Image;
+
+use base 'CXGN::Cview::Chromosome';
+
+use GD;
+
+sub new { 
+    my $class = shift;
+    my $id = shift;
+    my $file = shift;
+
+    my $self = $class->SUPER::new(@_);
+
+    
+    $self->{chr_image}  = GD::Image->newFromPng($file);
+
+    if (!defined($self->{chr_image})) { die "Unable to load image file $file."; }
+
+    return $self;
+}
+
+sub draw_chromosome { 
+    my $self = shift;
+    
+    $self->render(@_);
+}
+sub render { 
+    my $self = shift;
+    my $image = shift;
+
+    print STDERR "Rendering chromosome...\n";
+    my $ratio = $self->{chr_image}->width()/$self->{chr_image}->height();
+    my $halfwidth = int(($self->get_height()* $ratio)/2);
+    $image->copyResized($self->{chr_image}, 
+			$self->get_X()-$halfwidth, 
+			$self->get_Y(), 
+			0, 
+			0, 
+			$self->get_height() * $ratio,
+			$self->get_height(),
+			$self->{chr_image}->width(), 
+			$self->{chr_image}->height()
+	);
+
+    $self->draw_caption($image);
+    print STDERR "Done.\n";
+
+}
+
+1;
---
+++ lib/CXGN/Cview/Map/SGN/Image.pm
@@ -0,0 +1,97 @@
+
+use Modern::Perl;
+
+package CXGN::Cview::Map::SGN::Image;
+
+use base 'CXGN::Cview::Map';
+use File::Basename;
+
+use CXGN::Cview::Chromosome::Image;
+
+sub new { 
+    my $class = shift;
+    my $dbh = shift;
+    my $id = shift;
+    my @files = @_;
+
+
+    my $self = $class->SUPER::new($dbh);
+    
+    $self->set_id($id);
+    $self->set_units('%');
+    @{$self->{files}} = @files;
+
+    my @chr_names = ();
+    foreach my $f (@files) { 
+	push @chr_names, basename($f, ".png");
+    }
+    $self->set_chromosome_names(@chr_names);
+    $self->set_chromosome_count(scalar(@chr_names));
+    
+    my @chr_lens;
+    foreach my $f (@files) { 
+	# use length in pixels as map length
+	my $length = `file $f`;
+	$length =~ s/.*x (.\d+),.*/$1/;
+	push @chr_lens, $length;
+    }
+	
+    $self->set_chromosome_lengths(@chr_lens);
+	
+    
+    return $self;
+
+}
+
+sub can_overlay { 
+    return 0;
+}
+
+sub get_chromosome { 
+    my $self = shift;
+    my $chr_nr = shift;
+
+    print STDERR "**** pachy get_chromosome $chr_nr\n";
+
+    my $file_index;
+    my @chr_names = $self->get_chromosome_names();
+    for (my $n=0; $n< (@chr_names); $n++) { 
+	if ($chr_names[$n] eq $chr_nr) { 
+	    $file_index = $n;
+	}
+    }
+    my $chr = CXGN::Cview::Chromosome::Image->new($chr_nr, $self->{files}->[$file_index]);
+
+    $chr->set_length(100);
+    $chr->_calculate_chromosome_length();
+    $chr->set_caption($chr_nr);
+    
+    
+    return $chr;
+
+}
+
+sub get_chromosome_count { 
+    my $self = shift;
+    return scalar(@{$self->{files}});
+}
+
+
+sub get_chromosome_connections { 
+    my $self = shift;
+    my $chr_nr = shift;
+
+    $chr_nr =~ s/.*(\d+).*/$1/;
+    return { map_version_id         => 25,
+	     lg_name        => $chr_nr,
+	     marker_count   => '?',
+	     short_name     => 'FISH map',
+    };
+}
+
+
+sub show_ruler{ 
+    return 0;
+}
+
+1;
