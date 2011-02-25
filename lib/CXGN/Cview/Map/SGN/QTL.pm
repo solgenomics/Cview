32ef00f218a6754a31490dacfe19f5d4d1e5e4b3
mueller <lam87@cornell.edu>
2011-02-24 23:27:48 -0500

    a first version of a working QTL map object.

--- lib/CXGN/Cview/Map/SGN/QTL.pm
+++ lib/CXGN/Cview/Map/SGN/QTL.pm
@@ -23,40 +23,65 @@ sub get_chromosome {
 
     my $side = "right";
 
-    my $qtl = CXGN::Cview::Marker::QTL->new($chr);
-    $qtl->set_range_coords(20,30);
-    $qtl->set_marker_name("blabla");
-    $qtl->get_label()->set_name("blabla");
+    my $q = "SELECT marker_alias.marker_id, marker_alias.alias, marker_location.position_north, marker_location.position_south, locus_marker.locus_id FROM sgn.marker_alias join  sgn.marker_experiment using(marker_id) join sgn.marker_location using(location_id) join sgn.linkage_group using(lg_id) left join phenome.locus_marker using(marker_id)  where lg_name=? and marker_location.map_version_id=? and protocol=?";
 
-    $qtl->set_label_side($side);
-#    $qtl->get_label()->set_align_side("default");
-    $qtl->set_url("http://google.com");
-    $qtl->set_tooltip("congratulations!");
-    $qtl->set_fill_color(0, 0, 255);
-#    $qtl->set_show_tick(1);
-    
-    $chr->add_marker($qtl);
+    my $h = $self->get_dbh()->prepare($q);
+    $h->execute($chr_nr, $self->get_id(), "QTL");
 
-    my $rl = CXGN::Cview::Marker::RangeMarker->new($chr);
-    $rl->set_marker_name("RangeMarker");
-    $rl->set_color(244,0, 0);
-    $rl->hilite();
-    $rl->get_label()->set_name('foo');
-    $rl->get_label()->set_hilited(1);
-    $rl->set_range_coords(40, 50);
-    $rl->set_label_side($side);
-    $rl->get_label()->set_align_side("auto");
-    $rl->show_label();
-    $rl->set_url('http://google.com');
-    $rl->unhide();
+    while (my ($marker_id, $name, $north, $south, $locus_id) = $h->fetchrow_array()) { 
+	
+	#print STDERR "Adding marker $marker_id, $name, $north, $south\n";
+	my $qtl = CXGN::Cview::Marker::QTL->new($chr, $marker_id);
+	$qtl->set_range_coords($north,$south);
+	$qtl->set_marker_name($name);
+	$qtl->get_label()->set_name($name);
+	$qtl->get_label()->set_vertical_stacking_spacing(2);
+	$qtl->get_label()->set_stacking_height(10);
 
-    $chr->add_marker($rl);
+	$qtl->set_label_side($side);
 
+	if ($locus_id) { 
+	    $qtl->set_url("/phenome/locus_display.pl?locus_id=$locus_id");
+	}
+	$qtl->set_tooltip($name ." (".(sprintf "%5.2f", $north)."-".(sprintf "%5.2f", $south)." cM)");
+
+	if ($name =~ /blight/i) { 
+	    $qtl->set_fill_color(0, 0, 255);
+	}
+	elsif ($name =~ /maturity/i) { 
+	    $qtl->set_fill_color(0, 255, 0);
+	}
+	else { 
+	    $qtl->set_fill_color(0, 255, 255);
+	}
+	$chr->add_marker($qtl);
+    }
+
+    $chr->distribute_label_stacking();
     $chr->sort_markers();
 
     return $chr;
 
 }
 
+
+sub get_overview_chromosome { 
+    my $self = shift;
+    my $chr_nr = shift;
+    
+    my $chr = $self->SUPER::get_overview_chromosome($chr_nr);
+
+    foreach my $m ($chr->get_markers()) { 
+	$m->get_label()->set_stacking_height(5);
+	print STDERR "STACKING LEVEL: ".$m->get_label()->get_stacking_level()."\n";
+
+    }
+
+    $chr->distribute_label_stacking();
+    
+
+    return $chr;
+}
+    
 1;
 
